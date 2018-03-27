f1197449e15c642737f883bf70d8545249db296d
Andy Chosak <andy@chosak.org>
2018-03-27 12:34:45 -0400

    docker-compose v2 for docker 1.11 compatibility

--- convert.sh
+++ convert.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+# Fail if anything fails.
+set -e
+
 # Run load script in MySQL container. This loads MySQL container's database.
 # Requires a local file named "production_complete.sql".
-docker-compose exec mysql /host/mysql_load.sh
+docker-compose exec -T mysql /host/mysql_load.sh
 
 # Use pgloader to copy MySQL data over to Postgres.
-docker-compose run pgloader pgloader --debug --client-min-messages=debug /host/pgloader.conf
+docker-compose run -T pgloader pgloader --debug --client-min-messages=debug /host/pgloader.conf
 
 # Dump Postgres data to a new export called postgres.sql.
-docker-compose exec postgres /host/postgres_dump.sh
+docker-compose exec -T postgres /host/postgres_dump.sh
